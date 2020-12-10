6b4b7e11ef7182c529ac209cb0c979bf66dd113f
Andrey Shipul work <sh00ter@tut.by>
2020-12-10 16:28:42 +0300

    Two methods request param

--- src/main/Java/by/shipul/spring/controllers/FirstController.java
+++ src/main/Java/by/shipul/spring/controllers/FirstController.java
@@ -3,18 +3,22 @@ package by.shipul.spring.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage() {
+    public String helloPage( @RequestParam("name") String name ) {
         return "first/hello";
     }
 
     @GetMapping("/good-bye")
-    public String goodbyePage() {
+    public String goodbyePage(HttpServletRequest request) {
+        String name = request.getParameter("name");
         return "first/good-bye";
     }
 }
